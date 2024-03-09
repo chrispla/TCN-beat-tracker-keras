f79ee7fe1119beec7f0c9087fe92cc356dc06416
chrispla <cplachouras@gmail.com>
2024-03-09 04:02:40 +0000

    remove tqdm

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import librosa
 import numpy as np
-from tqdm import tqdm
 
 with open("data/ballroom/audio/allBallroomFiles", "r") as f:
     audio_files = f.readlines()
@@ -13,9 +12,11 @@ with open("data/ballroom/audio/allBallroomFiles", "r") as f:
 spectrogram_dir = Path("data/ballroom/spectrograms")
 spectrogram_dir.mkdir(exist_ok=True)
 
-for audio_file in tqdm(audio_files, total=len(audio_files)):
+for audio_file in audio_files:
     y, sr = librosa.load(f"data/ballroom/audio/{audio_file}", sr=44100)
     mel_spectrogram = librosa.feature.melspectrogram(
         y=y, sr=sr, n_mels=81, hop_length=441, n_fft=2048
     )
     np.save(spectrogram_dir / f"{Path(audio_file).stem}.npy", mel_spectrogram)
+
+print("Spectrograms computed and saved to", spectrogram_dir)
